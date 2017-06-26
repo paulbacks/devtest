ac883ea1562fed45610e54bf72facad6ac6f65f1
paba <p.backs@bluedesk.nl>
2017-06-26 23:03:15 +0200

    completed the assigment.

--- library/src/Application/Main.php
+++ library/src/Application/Main.php
@@ -5,33 +5,179 @@ namespace Application;
 class Main 
 {
     /**
+     * the data set
+     *
      * @var array
      */
 	protected $data;
 
     /**
+     * set the data set to work with.
+     *
      * @param array $data
      */
 	public function __construct(array $data)
-	{
-		$this->setData($data);
-	}
-
-    /**
-     * @param array $data
-     */
-	public function setData(array $data)
 	{
 		$this->data = $data;
 	}
 
     /**
-     * Your implementation
+     * returns all males form the data set.
      *
-     * @todo create your implementation
+     * @return string
      */
-	public function myFunction()
-	{
-		//... remove this function
+	public function allMales(){
+        //define empty variables that will be used in this function
+        $allMales = array();
+
+	    //loop through all the people, and if their male add them to a array.
+        foreach ($this->data['people'] as $person){
+            if($person['gender'] == 'male'){
+                array_push($allMales, $person);
+            }
+        }
+
+        //get all array keys, this will be used to place it in an table.
+        $keys = array_keys($this->data['people'][1]);
+
+        //place the data in a array
+        $allMalesInTable = $this->toTable($keys, $allMales);
+
+        return $allMalesInTable;
+    }
+
+    /**
+     * this wil return a table with all modals with the $filterValue in it
+     *
+     * @param $filterValue
+     * @return string
+     */
+    public function allCarModalsWithFilter($filterValue){
+        //define empty variables that will be used in this function
+        $allCarModalsWithTheFilterValue = array();
+
+        //loops trough all car brands and modals
+        foreach ($this->data['cars']['brands'] as $brands){
+            foreach ($brands['models'] as $modal){
+                //if the modal has the filterValue in the string add it to the array.
+                if(strstr($modal, $filterValue)){
+                    array_push($allCarModalsWithTheFilterValue, $modal);
+                }
+            }
+        }
+
+        //set key, in this case the key is static and will not be based on the data source (their all modal numbers so it doesn't really matter. =D)
+        $key = 'modals';
+
+        //place the data in a array
+        $allCarModalsWithTheFilterValueInTable = $this->toTable($key, $allCarModalsWithTheFilterValue);
+
+        return $allCarModalsWithTheFilterValueInTable;
+    }
+
+    /**
+     * returns all numbers greater than the $filterValue
+     *
+     * @param $filterValue
+     * @return string
+     */
+    public function numberGreaterThan($filterValue){
+        //define empty variables that will be used in this function
+        $numbers = array();
+
+        foreach ($this->data['numbers'] as $number){
+            if($number > $filterValue){
+                array_push($numbers, $number);
+            }
+        }
+
+        //get all array keys, this will be used to place it in an table.
+        $keys = 'numbers';
+
+        //place the data in a array
+        $numbersInTable = $this->toTable($keys, $numbers);
+
+        return $numbersInTable;
+    }
+
+    /**
+     * returns all phone numbers based on the $filterValue
+     *
+     * @param $filterValue
+     * @return string
+     */
+    public function allPhoneNumbersEndingOn($filterValue){
+        //define empty variables that will be used in this function
+        $filteredPhoneNumbers = array();
+
+        //checks the filter length
+        $filterLength = strlen($filterValue);
+
+        foreach ($this->data['phone-numbers'] as $phoneNumber){
+            if(substr($phoneNumber, -1, $filterLength) == $filterValue){
+                array_push($filteredPhoneNumbers, $phoneNumber);
+            }
+        }
+
+        //get all array keys, this will be used to place it in an table.
+        $keys = 'phone-number';
+
+        //place the data in a array
+        $filteredPhoneNumbersInTable = $this->toTable($keys, $filteredPhoneNumbers);
+
+        return $filteredPhoneNumbersInTable;
+    }
+
+    /**
+     * builds a table with the provided keys and data.
+     * maybe should move this to a template class.
+     *
+     * @param $keys
+     * @param $dataGroup
+     * @return string
+     */
+    private function toTable($keys, $dataGroup){
+        //checks if the $keys, is a string
+        if(is_string($keys)){
+            $keys = explode(" ", $keys);
+        }
+
+        //the start of the table
+	    $tableWithData = '
+            <div class="table">
+            <table>
+            <thead>
+            <tr>
+        ';
+	    //table heads
+        foreach ($keys as $key){
+            $tableWithData .= '<th>' . $key . '</th>';
+        }
+        $tableWithData .= '
+            </tr>
+            </thead>
+            <tbody>
+        ';
+        //table content
+        foreach ($dataGroup as $data){
+            $tableWithData .= '<tr>';
+            //checks if the data is a string, true : display, else : loop through data set.
+            if(is_string($data) || is_numeric($data)){
+                $tableWithData .= '<th>' . $data . '</th>';
+            }
+            else{
+                foreach($data as $key) {
+                    $tableWithData .= '<th>' . $key . '</th>';
+                }
+            }
+            $tableWithData .=  '</th>';
+        }
+        $tableWithData .= '
+            </tbody>
+            </table>
+            </div>
+        ';
+
+        return $tableWithData;
 	}
 }
--- public/index.php
+++ public/index.php
@@ -26,37 +26,34 @@
 require_once __DIR__ . "/../autoload_init.php";
 $application = new Application\Main(include __DIR__ . '/../data/assessment.php');
 
+echo '<link rel="stylesheet" href="style.css" type="text/css">';
+echo '<div style="width: 100%">';
 /**
- * The following method will return all male people
- *
- * @todo implementation
+ * return all males in a table.
  */
-$application->myFunction();
+echo '<div style="width:25%; float: left;"><h1>All males</h1>';
+echo $application->allMales();
+echo '</div>';
 
 /**
- * The following method will return all car models containing 3
- *
- * @todo implementation
+ * return all car models containing a 3
  */
-$application->myFunction();
+echo '<div style="width:25%; float: left;"><h1>All modals with a 3</h1>';
+echo $application->allCarModalsWithFilter('3');
+echo '</div>';
 
 /**
- * The following method will return all numbers greater than or equal to 1000
- *
- * @todo implementation
+ * return all numbers greater than or equal to 1000
  */
-$application->myFunction();
+echo '<div style="width:25%; float: left;"><h1>All numbers greater than 999';
+echo $application->numberGreaterThan('999');
+echo '</div>';
 
 /**
- * The following method will return all phone numbers ending with 01
- *
- * @todo implementation
+ * return all phone numbers ending with 01
  */
-$application->myFunction();
+echo '<div style="width:25%; float: left;"><h1>All phone numbers ending with 01</h1>';
+echo $application->allPhoneNumbersEndingOn(01);
+echo '</div>';
 
-/**
- * You may remove this code 
- */
-echo <<<EOF
-	<h2>REYEZ! development test</h2>
-EOF;
+echo '</div>';
